bb3e9a714e68c87359925ff506affbf49493710d
Amir Alazadi <amir.alazadi@gmail.com>
2016-09-09 15:54:03 -0700

    DecimalToRoman - Red - 1

---
+++ DecimalToRomanTester/DecimalToRomanUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DecimalToRomanTester
+{
+    [TestClass]
+    public class DecimalToRomanUnitTests
+    {
+        [TestMethod]
+        public void DecimalToRomanConvertor_When1_ReturnI()
+        {
+            //Arrange
+            int decimalNum = 1;
+
+            //Act
+            string roman = DecimalToRomanConvertor.ConvertToRoman(decimalNum);
+
+            //Assert
+        }
+    }
+}
