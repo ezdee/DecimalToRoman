47706077e9c631d9083e0577f099b59b62570d6e
Amir Alazadi <amir.alazadi@gmail.com>
2016-09-09 16:43:14 -0700

    DecimalToRoman - Green

--- DecimalToRomanLibrary/DecimalToRomanConvertor.cs
+++ DecimalToRomanLibrary/DecimalToRomanConvertor.cs
@@ -20,7 +20,8 @@ namespace DecimalToRomanLibrary
                     {40,"XL"},
                     {50,"L"},
                     {90,"XC"},
-                    {100,"C"}
+                    {100,"C"},
+                    {400,"CD"}
                 };
 
             return roman[decimalNum];
